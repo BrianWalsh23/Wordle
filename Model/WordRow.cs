79e1a6c131140ee9eb9a61c2ec8844d930720126
BrianWalsh23 <G00424410@atu.ie>
2024-01-02 15:24:10 +0000

    AppShell

--- Model/WordRow.cs
+++ Model/WordRow.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 
-namespace Wordle.Model
+namespace Wordle.Model;
+
+public class WordRow
 {
-    internal class WordRow
+    public WordRow()
     {
+        Letters = new Letter[5]
+        {
+            new Letter(),
+            new Letter(),
+            new Letter(),
+            new Letter(),
+            new Letter(),
+        };
     }
+    public Letter[] Letters { get; set; }
+
+    //Are letters valid Green yes. Yellow is yes but in wrong position.
+    public bool Validate(char[] correctAnswer)
+    {
+        int count = 0;
+
+        for (int i = 0; i < Letters.Length; i++)
+        {
+            var letter = Letters[i];
+            if (letter.Input == correctAnswer[i])
+            {
+                letter.Color = Colors.Green;
+                count++;
+            }
+            else if (correctAnswer.Contains(letter.Input))
+            {
+                letter.Color = Colors.Yellow;
+            }
+            else
+            {
+                letter.Color = Colors.Gray;
+            }
+        }
+        return count == 5;
+    }
+}
+
+public partial class Letter : ObservableObject
+{
+    public Letter()
+    {
+        Color = Colors.Black;
+    }
+
+    [ObservableProperty]
+    private char input;
+
+    [ObservableProperty]
+    private Color color;
+
+
 }
